7560374c7bb13f7fad537acdd628e4e1826e0f80
Fred Emilio Schlesinger <fesgo@rocketmail.com>
2025-08-29 16:01:59 +0200

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-red-600 text-3xl font-bold underline'>
-        Hello world!
-      </h1>
+    <div className='p-4'>
+      <div className='flex flex-col gap-y-4'>
+        <div>
+          <h1 className='text-2xl text-rose-700 underline font-bold'>Home page</h1>
+        </div>
+        <div>
+          <Button variant='elevated'>I am a button</Button>
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value='I am a textarea' />
+        </div>
+      </div>
     </div>
   );
 }
