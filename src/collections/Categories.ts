04e0091d078d3aae0bd45dfe098e77222424a3d7
Fred Emilio Schlesinger <fesgo@rocketmail.com>
2025-09-01 21:20:34 +0200

    05: search filters

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,4 +1,4 @@
-import { CollectionConfig } from "payload";
+import type { CollectionConfig } from "payload";
 
 export const Categories: CollectionConfig = {
   slug: "categories",
@@ -8,5 +8,29 @@ export const Categories: CollectionConfig = {
       type: "text",
       required: true,
     },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: true,
+    }
   ],
 };
